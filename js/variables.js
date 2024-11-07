04ab2887634536de1c58bccfdc7b044e396c56d3
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2024-11-07 15:48:49 -0500

    Adding LANE variable

--- js/variables.js
+++ js/variables.js
@@ -33,3 +33,38 @@ var LANCS = L.geoJson(LANCS, {
         });
     }
 })
+
+var LANE = L.geoJson(LANE, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, {
+            color: '#808080',
+            weight: 1,
+            fillColor: '#000066',
+            fillOpacity: .8,
+            radius: 10
+        });
+    },
+    onEachFeature: function (feature, layer) {
+        const props = feature.properties
+        const popup = `
+                <b>${props.Informant}</b> (<b>${props.Project}</b>)
+                <br>County: ${props.County}<br>
+            `
+        layer.bindTooltip(popup, {
+            className: 'tool-informant'
+        });
+
+        layer.on('mouseover', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#000066'
+            });
+        });
+        layer.on('mouseout', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#000066'
+            });
+        });
+    }
+})
