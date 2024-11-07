52f74ad7b654f6d4cc2f319b1ac424c2e683d6c2
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2024-11-07 15:21:46 -0500

    starting variable

---
+++ js/variables.js
@@ -0,0 +1,35 @@
+// Adding variables for checkboxes 
+var LANCS = L.geoJson(LANCS, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, {
+            color: '#808080',
+            weight: 1,
+            fillColor: '#000066',
+            fillOpacity: .8,
+            radius: 10
+        });
+    },
+    onEachFeature: function (feature, layer) {
+        const props = feature.properties
+        const popup = `
+                <b>${props.Informant}</b>
+                <br>County: ${props.County}<br>
+            `
+        layer.bindTooltip(popup, {
+            className: 'tool-informant'
+        });
+
+        layer.on('mouseover', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#000066'
+            });
+        });
+        layer.on('mouseout', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#000066'
+            });
+        });
+    }
+})
