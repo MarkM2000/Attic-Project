25a15f6352a9c72a4b2c306bca4964bc684ba94f
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2024-11-08 14:15:02 -0500

    add ethnicity variables

--- js/variables.js
+++ js/variables.js
@@ -1,4 +1,5 @@
 // Adding variables for checkboxes 
+/// Projects variables
 var LANCS = L.geoJson(LANCS, {
     pointToLayer: function (feature, latlng) {
         return L.circleMarker(latlng, {
@@ -49,6 +50,77 @@ var LANE = L.geoJson(LANE, {
         const popup = `
                 <b>${props.Informant}</b> (<b>${props.Project}</b>)
                 <br>County: ${props.County}<br>
+            `
+        layer.bindTooltip(popup, {
+            className: 'tool-informant'
+        });
+
+        layer.on('mouseover', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#000066'
+            });
+        });
+        layer.on('mouseout', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#000066'
+            });
+        });
+    }
+})
+
+/// Ethnicity variables
+var white = L.geoJson(white, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, {
+            color: '#808080',
+            weight: 1,
+            fillColor: '#000066',
+            fillOpacity: .8,
+            radius: 10
+        });
+    },
+    onEachFeature: function (feature, layer) {
+        const props = feature.properties
+        const popup = `
+                <b>${props.Informant}</b>
+                <br>Ethnicity: ${props.Ethnicity}<br> (${props.Sex}<br>)
+            `
+        layer.bindTooltip(popup, {
+            className: 'tool-informant'
+        });
+
+        layer.on('mouseover', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#000066'
+            });
+        });
+        layer.on('mouseout', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#000066'
+            });
+        });
+    }
+})
+
+var black = L.geoJson(black, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, {
+            color: '#808080',
+            weight: 1,
+            fillColor: '#000066',
+            fillOpacity: .8,
+            radius: 10
+        });
+    },
+    onEachFeature: function (feature, layer) {
+        const props = feature.properties
+        const popup = `
+                <b>${props.Informant}</b>
+                <br>Ethnicity: ${props.Ethnicity}<br> (${props.Sex}<br>)
             `
         layer.bindTooltip(popup, {
             className: 'tool-informant'
