24de15edafc0eeb173914050dcdd6652911b902b
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2024-11-08 14:13:28 -0500

    Adding ethnicity js files

---
+++ assets/ethnicity/black.js
@@ -0,0 +1,149 @@
+var black = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "IL12LN",
+          "Sex": "Male",
+          "Ethnicity ": "Black or African American",
+          "County": "Beverly Ridge"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -87.6563,
+            41.7014
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "KY7NA",
+          "Sex": "Male",
+          "Ethnicity ": "Black or African American",
+          "County": "Jeffersontown"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -85.5644,
+            38.1942
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "IL12JN",
+          "Sex": "Male",
+          "Ethnicity ": "Black or African American",
+          "County": "Beverly Ridge"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -87.6563,
+            41.7014
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "ME78",
+          "Sex": "Male",
+          "Ethnicity ": "Black or African American",
+          "County": "Cumberland"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -70.267,
+            43.667
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "KY17NA",
+          "Sex": "Male",
+          "Ethnicity ": "Black or African American",
+          "County": "Jonestown"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -84.4966,
+            37.977
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "CT183",
+          "Sex": "Male",
+          "Ethnicity ": "Black or African American",
+          "County": "Litchfield"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -73.45,
+            41.8667
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "IL12HN",
+          "Sex": "Male",
+          "Ethnicity ": "Black or African American",
+          "County": "Chicago"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -87.6298,
+            41.8781
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "KY7NE",
+          "Sex": "Female",
+          "Ethnicity ": "Black or African American",
+          "County": "Windy Hills"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -85.3816,
+            38.16
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "WI17NA",
+          "Sex": "Male",
+          "Ethnicity ": "Black or African American",
+          "County": "South Lancaster"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -90.7343,
+            42.8029
+          ]
+        }
+      }
+    ]
+  }
