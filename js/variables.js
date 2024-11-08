71735f55d5ae671f5a58762e1455c2e831b3a399
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2024-11-08 14:53:49 -0500

    add cerical variable

--- js/variables.js
+++ js/variables.js
@@ -140,3 +140,39 @@ var black = L.geoJson(black, {
         });
     }
 })
+
+/// Occupation variables
+var clerical = L.geoJson(clerical, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, {
+            color: '#808080',
+            weight: 1,
+            fillColor: '#000066',
+            fillOpacity: .8,
+            radius: 10
+        });
+    },
+    onEachFeature: function (feature, layer) {
+        const props = feature.properties
+        const popup = `
+                <b>${props.Informant}</b>
+                <br>Occupation: ${props.Occupation}<br> (<b>${props.Sex}</b>)
+            `
+        layer.bindTooltip(popup, {
+            className: 'tool-informant'
+        });
+
+        layer.on('mouseover', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#000066'
+            });
+        });
+        layer.on('mouseout', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#000066'
+            });
+        });
+    }
+})
