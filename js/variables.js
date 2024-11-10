c76e930e0b71c681a95dd5d69c635fcd19e1b695
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2024-11-09 19:54:06 -0500

    add variable

--- js/variables.js
+++ js/variables.js
@@ -211,3 +211,38 @@ var farming = L.geoJson(farming, {
         });
     }
 })
+
+var occupation = L.geoJson(occupation, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, {
+            color: '#808080',
+            weight: 1,
+            fillColor: '#000066',
+            fillOpacity: .8,
+            radius: 10
+        });
+    },
+    onEachFeature: function (feature, layer) {
+        const props = feature.properties
+        const popup = `
+                <b>${props.Informant}</b>
+                <br>Occupation: ${props.Occupation}<br> (<b>${props.Sex}</b>)
+            `
+        layer.bindTooltip(popup, {
+            className: 'tool-informant'
+        });
+
+        layer.on('mouseover', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#000066'
+            });
+        });
+        layer.on('mouseout', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#000066'
+            });
+        });
+    }
+})
