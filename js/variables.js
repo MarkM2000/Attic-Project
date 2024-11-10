9af3d7f5708322bbc9809944f26671625c422785
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2024-11-09 20:19:11 -0500

    Adding age

--- js/variables.js
+++ js/variables.js
@@ -246,3 +246,39 @@ var occupation = L.geoJson(occupation, {
         });
     }
 })
+
+/// Age
+var age = L.geoJson(age, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, {
+            color: '#808080',
+            weight: 1,
+            fillColor: '#000066',
+            fillOpacity: .8,
+            radius: 10
+        });
+    },
+    onEachFeature: function (feature, layer) {
+        const props = feature.properties
+        const popup = `
+                <b>${props.Informant}</b> (<b>${props.Sex}</b>)
+                <br>Age: ${props.Age}<br>
+            `
+        layer.bindTooltip(popup, {
+            className: 'tool-informant'
+        });
+
+        layer.on('mouseover', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#000066'
+            });
+        });
+        layer.on('mouseout', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#000066'
+            });
+        });
+    }
+})
