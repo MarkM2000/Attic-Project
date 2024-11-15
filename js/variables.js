b0fac124bb9d848612c058d47d951d486c728f87
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2024-11-14 21:52:05 -0500

    Add var upstairs

--- js/variables.js
+++ js/variables.js
@@ -459,3 +459,38 @@ var target_attic = L.geoJson(target_attic, {
         });
     }
 })
+
+var upstairs = L.geoJson(upstairs, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, {
+            color: '#1f78b4',
+            weight: 1,
+            fillColor: '#0D98BA',
+            fillOpacity: .8,
+            radius: 10
+        });
+    },
+    onEachFeature: function (feature, layer) {
+        const props = feature.properties
+        const popup = `
+                <b>${props.Informant}</b>
+                <br>Attic type: ${props.Target_attic}<br>
+            `
+        layer.bindTooltip(popup, {
+            className: 'tool-informant'
+        });
+
+        layer.on('mouseover', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#0D98BA'
+            });
+        });
+        layer.on('mouseout', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#0D98BA'
+            });
+        });
+    }
+})
