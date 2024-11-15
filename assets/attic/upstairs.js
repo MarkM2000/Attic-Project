96716a39d390fe515222ea2c2939f59d51de26bb
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2024-11-14 21:49:47 -0500

    Create upstairs.js

---
+++ assets/attic/upstairs.js
@@ -0,0 +1,425 @@
+var upstairs = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "CT233",
+          "County": "New London",
+          "Target_attic": "upstair, attic, sug. garret, up garret"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -72.1425,
+            41.6197
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "IL45A",
+          "County": "Witt",
+          "Target_attic": "upstairs"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -89.214,
+            39.1516
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "IN22A",
+          "County": "White River",
+          "Target_attic": "upstairs"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -87.5594,
+            38.4386
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "KY2B",
+          "County": "Sebree",
+          "Target_attic": "upstairs"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -87.5286,
+            37.607
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "KY12B",
+          "County": "Salt Lick",
+          "Target_attic": "upstairs"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -83.3658,
+            38.71
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "KY24B",
+          "County": "Cadiz",
+          "Target_attic": "upstairs"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -87.8353,
+            36.865
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "KY30C",
+          "County": "Lerose",
+          "Target_attic": "upstairs"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -83.3656,
+            37.2856
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "MI9B",
+          "County": "Alpena",
+          "Target_attic": "upstairs"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -83.4328,
+            45.0617
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "MI10A",
+          "County": "Summit",
+          "Target_attic": "upstairs"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -86.2454,
+            43.5052
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "KY21B",
+          "County": "Winifred",
+          "Target_attic": "upstairs room, upstairs, a loft"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -82.8546,
+            37.9462
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "KY26B",
+          "County": "Mt. Zion",
+          "Target_attic": "upstairs, a attic"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -86.0572,
+            36.6831
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "KY19B",
+          "County": "Owingsville",
+          "Target_attic": "upstairs, a loft"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -83.7641,
+            38.1448
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "IL6A",
+          "County": "Mt. Carroll",
+          "Target_attic": "upstairs, an attic, the attic, garret"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -89.9782,
+            42.095
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "IL6C",
+          "County": "Mt. Pleasant",
+          "Target_attic": "upstairs, attic"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -89.5533,
+            41.4759
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "KY24A",
+          "County": "Blue Spring",
+          "Target_attic": "upstairs, attic"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -87.9375,
+            36.8289
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "MI6A",
+          "County": "St. James",
+          "Target_attic": "upstairs, attic"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -85.5226,
+            45.7454
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "OH23A",
+          "County": "Ashland ",
+          "Target_attic": "upstairs, attic"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -82.3182,
+            40.8687
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "KY20A",
+          "County": "Frenchburg",
+          "Target_attic": "upstairs, attic, c. a loft"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -83.6257,
+            37.9509
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "MI9A",
+          "County": "Alpena",
+          "Target_attic": "upstairs, attic, c. garret"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -83.4328,
+            45.0617
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "KY30E",
+          "County": "Cow Creek",
+          "Target_attic": "upstairs, attic, upper room"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -83.3638,
+            37.2619
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "KY8B",
+          "County": "Monterey",
+          "Target_attic": "upstairs, c. a loft"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -84.5221,
+            38.2521
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "KY24C",
+          "County": "Cadiz",
+          "Target_attic": "upstairs, lofts, the attic"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -87.8353,
+            36.865
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "IL46C",
+          "County": "Vandalia",
+          "Target_attic": "upstairs, the loft"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -89.0937,
+            38.9606
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "KY18A",
+          "County": "Irvine",
+          "Target_attic": "upstairs, the loft"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -83.9738,
+            37.7006
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "KY22A",
+          "County": "Hailwell",
+          "Target_attic": "upstairs, the loft"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -89.1059,
+            36.6764
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "KY27B",
+          "County": "Somerset",
+          "Target_attic": "upstairs, the loft"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -84.6041,
+            37.092
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "KY34C",
+          "County": "Kingdom Come",
+          "Target_attic": "upstairs, the loft"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -82.8803,
+            37.0804
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "KY27A",
+          "County": "Goodwater",
+          "Target_attic": "upstairs, the loft, garret"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -84.3733,
+            37.0754
+          ]
+        }
+      }
+    ]
+  }
